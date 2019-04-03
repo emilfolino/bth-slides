7cce1be945403fbedda07162d5a41864fd63bdbc
Emil Folino <emilfolino@gmail.com>
2019-04-03 09:34:14 +0200

    Added text and better syntax highlighting

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
 import slideorama from "slideorama";
 
+import hljs from 'highlight.js/lib/highlight';
+import javascript from 'highlight.js/lib/languages/javascript';
+import html from 'highlight.js/lib/languages/xml';
+import css from 'highlight.js/lib/languages/css';
+import python from 'highlight.js/lib/languages/python';
+import cpp from 'highlight.js/lib/languages/cpp';
+import bash from 'highlight.js/lib/languages/bash';
+import java from 'highlight.js/lib/languages/java';
+
+import 'highlight.js/styles/monokai-sublime.css';
+
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('html', html);
+hljs.registerLanguage('css', css);
+hljs.registerLanguage('python', python);
+hljs.registerLanguage('cpp', cpp);
+hljs.registerLanguage('bash', bash);
+hljs.registerLanguage('java', java);
+
 slideorama.init("presentation", "slide-container");
+hljs.initHighlightingOnLoad();
